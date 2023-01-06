8e7d278dfd39332801ad7bf302c44764b905545f
Slava Tenyaev <61147580+sltn011@users.noreply.github.com>
2023-01-07 00:22:55 +0300

    ...

---
+++ src/Gateway/src/main/java/ru/RSOI/Gateway/ExceptionAdvice.java
@@ -0,0 +1,20 @@
+package ru.RSOI.Gateway;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleControllerException(AccessDeniedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), UNAUTHORIZED);
+    }
+
+}
